d5bf1284beac3d580285b7fa59944ddccb36eb91
Md Sakib <md2076379@gmail.com>
2024-09-04 01:15:50 +0530

    refactoring

--- src/main/java/com/shakkib/bloggingwebapp/config/ContentConfig.java
+++ src/main/java/com/shakkib/bloggingwebapp/config/ContentConfig.java
@@ -1,31 +1,12 @@
 package com.shakkib.bloggingwebapp.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
-import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
-import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
-import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
 public class ContentConfig implements WebMvcConfigurer {
-
-    @Bean
-    public ViewResolver viewResolver() {
-        final InternalResourceViewResolver bean = new InternalResourceViewResolver();
-        bean.setViewClass(JstlView.class);
-        bean.setPrefix("/WEB-INF/jsp/");
-        bean.setSuffix(".jsp");
-        return bean;
-    }
-
-    @Override
-    public void configureViewResolvers(ViewResolverRegistry registry) {
-        registry.viewResolver(viewResolver());
-    }
 
     @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
